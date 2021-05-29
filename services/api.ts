cbaae2bd35ed93db8a1e4b9fcd3cee0a1b857bc4
Gontran Bezerra <gontranbezerra@gmail.com>
2021-05-29 18:02:51 -0300

    feat: Request queue in Axios

--- services/api.ts
+++ services/api.ts
@@ -3,6 +3,8 @@ import { parseCookies, setCookie } from 'nookies';
 
 // const cookies = parseCookies();
 let cookies = parseCookies();
+let isRefreshing = false;
+let failedRequestQueue = [];
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
@@ -13,6 +15,7 @@ export const api = axios.create({
 
 api.interceptors.response.use(
   (response) => response,
+  //  async (error: AxiosError) => {  >>>> 1/2 - Dá erro, Axios não permite que essa função seje uma promisse
   (error: AxiosError) => {
     // console.log(error.response.status);
     if (error.response.status === 401) {
@@ -21,32 +24,58 @@ api.interceptors.response.use(
         cookies = parseCookies(); // pegar os cookies atualizados
 
         const { 'nextauth.refreshToken': refreshToken } = cookies;
+        const originalConfig = error.config;
 
-        api
-          .post('/refresh', {
-            refreshToken,
-          })
-          .then((response) => {
-            const { token } = response.data;
+        if (!isRefreshing) {
+          isRefreshing = true;
 
-            setCookie(undefined, 'nextauth.token', token, {
-              maxAge: 60 * 60 * 24 * 30, // 30 days
-              path: '/',
-            });
-            setCookie(
-              undefined,
-              'nextauth.refreshToken',
-              response.data.refreshToken,
-              {
+          api
+            .post('/refresh', {
+              refreshToken,
+            })
+            .then((response) => {
+              const { token } = response.data;
+
+              setCookie(undefined, 'nextauth.token', token, {
                 maxAge: 60 * 60 * 24 * 30, // 30 days
                 path: '/',
-              }
-            );
+              });
+              setCookie(
+                undefined,
+                'nextauth.refreshToken',
+                response.data.refreshToken,
+                {
+                  maxAge: 60 * 60 * 24 * 30, // 30 days
+                  path: '/',
+                }
+              );
 
-            api.defaults.headers['Authorization'] = `Bearer ${token}`;
+              api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+              failedRequestQueue.forEach((request) => request.onSuccess(token));
+              failedRequestQueue = [];
+            })
+            .catch((error) => {
+              failedRequestQueue.forEach((request) => request.onFailure(error));
+              failedRequestQueue = [];
+            })
+            .finally(() => {
+              isRefreshing = false;
+            });
+        }
+
+        // 2/2 - Pra resolver e contornar tem que retornar uma Promisse
+        return new Promise((resolve, reject) => {
+          failedRequestQueue.push({
+            onSuccess: (token: string) => {
+              (originalConfig.headers['Authorization'] = `Bearer ${token}`),
+                resolve(api(originalConfig));
+            },
+            onFailure: (error: AxiosError) => {
+              reject(error);
+            },
           });
-
-          
+        });
       } else {
         // delogar usuário
       }
