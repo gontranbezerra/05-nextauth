015a405e7a8cef44a6f2efaf1573c93a108fd96a
Gontran Bezerra <gontranbezerra@gmail.com>
2021-05-29 16:46:22 -0300

    feat: Retrieving authentication state

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
-import React, { createContext, ReactNode, useState } from 'react';
+import React, { createContext, ReactNode, useEffect, useState } from 'react';
 // import { useRouter } from 'next/router';
 import Router from 'next/router';
-import { setCookie } from 'nookies';
+import { setCookie, parseCookies } from 'nookies';
 
 import { api } from '../services/api';
 
@@ -33,6 +33,22 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user; // !!false;
 
+  useEffect(() => {
+    //   const cookies = parseCookies();
+    const { 'nextauth.token': token } = parseCookies();
+
+    if (token) {
+      api.get('/me').then((response) => {
+        const { email, permissions, roles } = response.data;
+        setUser({
+          email,
+          permissions,
+          roles,
+        });
+      });
+    }
+  }, []);
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const response = await api.post('sessions', {
@@ -46,13 +62,18 @@ export function AuthProvider({ children }: AuthProviderProps) {
         maxAge: 60 * 60 * 24 * 30, // 30 days
         path: '/',
       });
-      setCookie(undefined, 'nextauth.refreshToken', refreshToken);
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      });
 
       setUser({
         email,
         permissions,
         roles,
       });
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`;
 
       Router.push('/dashboard');
     } catch (error) {
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,15 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
+import { api } from '../services/api';
 
 function Dashboard() {
   const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+      api.get('/me').then(response => {
+          console.log(response)
+      })
+  }, [])
 
   return (
     <h1>
